1414caed19f948dfadfbc218a75f9f89e2781169
albertdemskoy <albertdemskoy1@gmail.com>
2024-06-16 00:19:15 +1000

    initial commit, can get some sports

--- hello-rust/src/main.rs
+++ hello-rust/src/main.rs
@@ -1,9 +1,21 @@
-fn main() {
-    let body = reqwest::get("https://www.rust-lang.org")
-        .await?
-        .text()
-        .await?;
+fn get_sports() {
 
-    println!("body = {body:?}");
-    
+    let ODDS_HOST_BASE = "https://api.the-odds-api.com"
+
+    let response = match reqwest::blocking::get("https://www.rust-lang.org") {
+        Ok(res) => res,
+        Err(e) => return,
+    };
+}
+
+fn main() {
+
+    let response = match reqwest::blocking::get("https://www.rust-lang.org") {
+        Ok(res) => res,
+        Err(e) => return,
+    };
+
+    let body = response.text();
+
+    println!("body = {body:?}");    
 }
