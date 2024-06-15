13eefba5cc9133713aa9c5f18b77e8cf97fd1078
albertdemskoy <albertdemskoy1@gmail.com>
2024-06-16 00:19:22 +1000

    get sports

--- hello-rust/src/main.rs
+++ hello-rust/src/main.rs
@@ -1,21 +1,42 @@
-fn get_sports() {
+use reqwest::Error;
+use reqwest::blocking::Response;
+use serde::{Deserialize};
 
-    let ODDS_HOST_BASE = "https://api.the-odds-api.com"
+#[derive(Deserialize, Debug)]
+struct Sport {
+    key: String,
+    group: String,
+    title: String,
+    description: String,
+    active: bool,
+    has_outrights: bool
+}
 
-    let response = match reqwest::blocking::get("https://www.rust-lang.org") {
-        Ok(res) => res,
-        Err(e) => return,
-    };
+fn get_sports() -> Result<Response, Error> {
+
+    let odds_host_base = "https://api.the-odds-api.com/v4/";
+    let sports_endpoint = "/sports/";
+    let full_url =  odds_host_base.to_owned() + sports_endpoint;
+    let params = [
+        ("apiKey", "74b4c29eb501524fc2d16ca5310de51c"),
+    ];
+
+    let url = reqwest::Url::parse_with_params(&full_url, &params).unwrap();
+    let res = reqwest::blocking::get(url);
+
+    return res;
 }
 
 fn main() {
 
-    let response = match reqwest::blocking::get("https://www.rust-lang.org") {
+    let response = match get_sports() {
         Ok(res) => res,
-        Err(e) => return,
+        Err(_e) => return,
     };
 
-    let body = response.text();
+    let headers = response.headers();
+    println!("headers:\n=======================\n{headers:?}"); 
 
-    println!("body = {body:?}");    
+    let body = response.json::<Vec<Sport>>().unwrap();
+    println!("body:\n===========================\n{body:?}");    
 }
