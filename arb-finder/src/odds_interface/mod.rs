b478dfe62e81a879269a0261ede65a338ab30860
albertdemskoy <albertdemskoy1@gmail.com>
2024-06-19 22:24:40 +1000

    give the project a bit of a spring clean

---
+++ arb-finder/src/odds_interface/mod.rs
@@ -0,0 +1,6 @@
+mod response_types;
+pub mod api_requests;
+mod util;
+
+const ODDS_HOST_BASE : &str = "https://api.the-odds-api.com/v4/";
+const API_KEY : &str  = "74b4c29eb501524fc2d16ca5310de51c";
