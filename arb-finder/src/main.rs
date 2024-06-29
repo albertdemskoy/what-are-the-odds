55323a60001f833cfd22c7e5979c60c9f3b6c321
albertdemskoy <albertdemskoy1@gmail.com>
2024-06-29 13:52:04 +1000

    add some files

--- arb-finder/src/main.rs
+++ arb-finder/src/main.rs
@@ -1,5 +1,5 @@
 use std::io;
-use odds_interface::{api_requests::{get_odds_for_sport_aus, get_sports}};
+use odds_interface::api_requests::{get_example_odds_file, get_key_usage, get_odds_for_sport_aus, get_sports};
 
 mod odds_interface;
 
@@ -8,6 +8,7 @@ fn main() {
     while num_inputs > 0 {
         println!("Available operations:");
         println!("==========================");
+        println!("a:   best odds pair testing");
         println!("s:   print in-season sports");
         println!("o:   odds for a sport of your chosing");
 
@@ -19,17 +20,34 @@ fn main() {
             .expect("Failed to read line");
         operation_choice = operation_choice.trim().to_string();
 
-        if operation_choice == "s" {
-            let mut sports = get_sports().expect("Failed to get sports");
-            println!("{sports:#?}")
-        } else if operation_choice == "o" {
-            let events = get_odds_for_sport_aus("./src/sportodds.json");
+        if operation_choice == "a" {
+            let events = get_example_odds_file("./src/soccerodds.json");
             for event in events {
                 event.get_best_odds_pair();
             }
+        } else if operation_choice == "s" {
+            let sports = get_sports().expect("Failed to get sports");
+            println!("{sports:#?}")
+        }  else if operation_choice == "o" {
+            println!("write your sport key of choice");
+
+            let mut sport_key = String::new();
+            io::stdin()
+            .read_line(&mut sport_key)
+            .expect("Failed to read line");
+            sport_key = sport_key.trim().to_string();
+
+            let event_raw = get_odds_for_sport_aus(&sport_key).expect("Failed to get odds for");
+            println!("{event_raw:?}")
         } else {
             println!("{operation_choice:#?} is not a valid choice!")
         }
+
+        let key_usage = get_key_usage();
+        match key_usage {
+            Some(x) => println!("requests remaining: {0}", x.requests_remaining),
+            None => println!("")
+        };
 
         num_inputs -= 1;
     }
