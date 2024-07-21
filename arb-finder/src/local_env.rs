739ffc030e7749a9dd0b1260a5b281609f540e94
albertdemskoy <albertdemskoy1@gmail.com>
2024-07-21 16:50:15 +1000

    remove api keys whoops

--- arb-finder/src/local_env.rs
+++ arb-finder/src/local_env.rs
@@ -4,6 +4,6 @@ pub struct LocalEnv<'a> {
 }
 
 pub const MY_ENV: LocalEnv = LocalEnv {
-    discord_api_key: "MTI2MjM3NDY1NTQ5MTMxMzc5NQ.GJBvsj.vrSJAg2c--AtHNRhGgk_nDL56ZVGJXGv9vzuiA",
-    odds_api_key: "74b4c29eb501524fc2d16ca5310de51c",
+    discord_api_key: "My Discord API Key",
+    odds_api_key: "My Odds API Key",
 };
