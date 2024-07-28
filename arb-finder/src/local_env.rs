d5f505cd8fe7e04dddbb3e8ed10f894e12689ad6
albertdemskoy <albertdemskoy1@gmail.com>
2024-07-28 13:52:51 +1000

    change local env interface

--- arb-finder/src/local_env.rs
+++ arb-finder/src/local_env.rs
@@ -1,9 +1,11 @@
 pub struct LocalEnv<'a> {
     pub odds_api_key: &'a str,
     pub discord_api_key: &'a str,
+    pub discord_channel_id: &'a str,
 }
 
 pub const MY_ENV: LocalEnv = LocalEnv {
     discord_api_key: "My Discord API Key",
     odds_api_key: "My Odds API Key",
+    discord_channel_id: "My Discord channel ID",
 };
